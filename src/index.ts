762d221d00a93e4753fb50acd214628e8615aab6
KingBean4903 <davidkanyango065@gmail.com>
2026-03-27 15:59:25 +0300

    pnpm clean

--- src/index.ts
+++ src/index.ts
@@ -46,7 +46,10 @@ fastify.get('/', async (request, reply) => {
 
 const start = async () => {
 								try {
-																await fastify.listen({ port: PORT })
+																await fastify.listen({ 
+																								port: PORT,
+																								host: '0.0.0.0'
+																})
 								} catch(err) {
 																fastify.log.error(err)
 																process.exit(1)
