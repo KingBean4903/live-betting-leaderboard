f3b2a38b6d89328e3cd77a42a547970c1e35a6f0
KingBean4903 <davidkanyango065@gmail.com>
2026-03-28 18:11:03 +0300

    Updated Redis

--- src/redis/redis.ts
+++ src/redis/redis.ts
@@ -1,10 +1,9 @@
 import { Redis } from 'ioredis'
 
-const redis = new Redis({
+export const redis = new Redis({
 								port: process.env.REDISPORT,
 								host: process.env.REDISHOST,
 								username: process.env.REDISUSER,
 								password: process.env.REDIS_PASSWORD
 });
 
-export redis;
