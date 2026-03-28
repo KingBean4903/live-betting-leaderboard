ac5b0c81c1afde2f526efa1868e126d62e23a113
KingBean4903 <davidkanyango065@gmail.com>
2026-03-28 19:13:50 +0300

    First cleanup

--- src/workers/consumer.ts
+++ src/workers/consumer.ts
@@ -2,7 +2,16 @@ import { Kafka } from 'kafkajs';
 import type { EachMessagePayload  } from 'kafkajs';
 import { workerData, parentPort  } from 'node:worker_threads';
 import fs from 'fs';
-import { redis } from '../redis/redis.js';
+import { Redis } from 'ioredis'
+
+const port: number = Number(process.env.REDISPORT) || 6379
+
+export const redis = new Redis({
+								port: port,
+								host: process.env.REDISHOST as string,
+								username: process.env.REDISUSER as string,
+								password: process.env.REDIS_PASSWORD as string
+});
 
 const VOTE_SCRIPT_LUA = fs.readFileSync('./dist/redis/vote.lua', 'utf8');
 const voteScriptSha = await redis.script("LOAD", VOTE_SCRIPT_LUA);
