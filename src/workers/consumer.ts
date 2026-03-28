f5e7ae02d4285d4aa42a935724b497495c5229e4
KingBean4903 <davidkanyango065@gmail.com>
2026-03-28 19:25:00 +0300

    Updated kafka

--- src/workers/consumer.ts
+++ src/workers/consumer.ts
@@ -6,7 +6,7 @@ import { Redis } from 'ioredis'
 
 const port: number = Number(process.env.REDISPORT) || 6379
 
-export const redis = new Redis({
+const redis = new Redis({
 								port: port,
 								host: process.env.REDISHOST as string,
 								username: process.env.REDISUSER as string,
